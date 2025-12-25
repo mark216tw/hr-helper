ff48611e31704b6885c63e6390c9fc3e041ae0f8
Mark <mark216tw@gmail.com>
2025-12-26 01:37:32 +0800

    build: Configure base path for `/hr-helper/` deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import autoprefixer from 'autoprefixer';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/hr-helper/',
     server: {
       port: 3000,
       host: '0.0.0.0',
